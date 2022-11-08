2d4797564e9c08b56961061b99c469d768374f58
Efrain <efraincabrera35@gmail.com>
2022-11-08 09:37:14 -0400

    rutas protegidas sin funcionar

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,29 +1,39 @@
-import { useSelector } from "react-redux"
-import { Routes,Route } from "react-router-dom"
-
-import { AuthRoutes } from "../auth/Routes/AuthRoutes"
-import { JournalRoutes } from "../journal/Routes/JournalRoutes"
-import { CheckingAuth } from "../ui"
-
-
+import { useSelector, useDispatch } from "react-redux";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { AuthRoutes } from "../auth/Routes/AuthRoutes";
+import { JournalRoutes } from "../journal/Routes/JournalRoutes";
+import { CheckingAuth } from "../ui";
+import { FirebaseAuth } from "../firebase/config";
+import { onAuthStateChanged } from "firebase/auth";
+import { login, logout } from "../store/auth/authSlice";
 
 export const AppRouter = () => {
+  const { status } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
 
-  const {status} = useSelector(state => state.auth);
+  useEffect(() => {
+    onAuthStateChanged(FirebaseAuth, async (user) => {
+      if (!user) return dispatch(logout());
+      const { uid, email, displayName, photoURL } = user;
+      dispatch(login({ uid, email, displayName, photoURL }));
+    });
+  }, []);
 
-  if (status === 'checking') {
-    return <CheckingAuth/>
+  if (status === "checking") {
+    return <CheckingAuth />;
   }
 
   return (
     <Routes>
+      {(status === 'authenticated' )
+        ? 
+        <Route path="/*" element={<JournalRoutes />} />
+        : 
+        <Route path="/auth/*" element={<AuthRoutes />} />
+      }
 
-        {/* Login y Registro */}
-        <Route path='/auth/*' element={<AuthRoutes/>}/>
-
-        {/* JournalApp */}
-        <Route path='/*' element={<JournalRoutes/>}/>
-        
+      <Route path='/*' element={<Navigate to='/auth/login' />}/>
     </Routes>
-  )
-}
+  );
+};
